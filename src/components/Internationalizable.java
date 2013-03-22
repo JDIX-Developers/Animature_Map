a59109403dfc83aa04eb19459fd980edb2c54a0a
Razican <admin@razican.com>
2013-03-22 13:56:51 +0100

    Se mejora la documentación de Internationalizable.

--- src/components/Internationalizable.java
+++ src/components/Internationalizable.java
@@ -1,11 +1,15 @@
 package components;
 
 /**
+ * Simple way to change the language of the component.
+ * 
  * @author Razican (Iban Eguia)
  */
 public interface Internationalizable {
 
 	/**
+	 * Changes the text of the component to reflect the new language setting.
+	 * 
 	 * @param newText The new text to set
 	 */
 	public void changeLanguage(String newText);
