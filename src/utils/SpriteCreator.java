65581fe1b0b9f339e6d537709caf2e417940b004
David Gonzalez <davidgonz_93@hotmail.com>
2013-04-15 16:59:50 +0200

    Nuevo Sprite
    He cambiado las imágenes para el MapEditor. También he puesto como
    comentario algunas líneas por ahora.

--- src/utils/SpriteCreator.java
+++ src/utils/SpriteCreator.java
@@ -12,16 +12,26 @@ import java.util.Map;
  */
 public class SpriteCreator {
 
+	// private String ANDABLE;
+	// private String CAMBIABLE;
+	// private String SALTABLE;
+	// private String NOANDABLE;
+	// private String TRANSPORTABLE;
+
 	/**
 	 * @param args Arguments for the application
 	 */
 	public static void main(String[] args)
 	{
-		byte[] arr = {0x00, (byte) 0x80};
+		byte[] arr = {0x00, (byte) 0x80,
+		/*
+		 * 0x00, 0x00, "CAMBIANTE", 0x01, 0x00, "ANDABLE", 0x02, 0x00,
+		 * "ANDABLE",
+		 */};
 
 		try
 		{
-			FileOutputStream st = new FileOutputStream("test.spr");
+			FileOutputStream st = new FileOutputStream("prueba.spr");
 			st.write(arr);
 			st.close();
 		}
@@ -32,27 +42,56 @@ public class SpriteCreator {
 
 		HashMap<String, Map.Entry<Byte, Byte>> m = new HashMap<>();
 
-		m.put("Soil/Grass", new SimpleImmutableEntry<Byte, Byte>(new Byte(
+		m.put("Soil/Weed", new SimpleImmutableEntry<Byte, Byte>(new Byte(
 		(byte) 0x00), new Byte((byte) 0x00)));
-		m.put("Soil/Soil", new SimpleImmutableEntry<Byte, Byte>(new Byte(
+		m.put("Soil/Grass1", new SimpleImmutableEntry<Byte, Byte>(new Byte(
 		(byte) 0x01), new Byte((byte) 0x00)));
-		m.put("Path/Top left", new SimpleImmutableEntry<Byte, Byte>(new Byte(
+		m.put("Soil/Grass2", new SimpleImmutableEntry<Byte, Byte>(new Byte(
 		(byte) 0x02), new Byte((byte) 0x00)));
-		m.put("Path/Top", new SimpleImmutableEntry<Byte, Byte>(new Byte(
+		m.put("Soil/Grass3", new SimpleImmutableEntry<Byte, Byte>(new Byte(
 		(byte) 0x03), new Byte((byte) 0x00)));
-		m.put("Path/Bottom Left", new SimpleImmutableEntry<Byte, Byte>(
-		new Byte((byte) 0x02), new Byte((byte) 0x01)));
+		m.put("Soil/Grass4", new SimpleImmutableEntry<Byte, Byte>(new Byte(
+		(byte) 0x04), new Byte((byte) 0x00)));
+		m.put("Path/Center", new SimpleImmutableEntry<Byte, Byte>(new Byte(
+		(byte) 0x00), new Byte((byte) 0x01)));
+		m.put("Path/Top", new SimpleImmutableEntry<Byte, Byte>(new Byte(
+		(byte) 0x01), new Byte((byte) 0x01)));
 		m.put("Path/Bottom", new SimpleImmutableEntry<Byte, Byte>(new Byte(
+		(byte) 0x02), new Byte((byte) 0x01)));
+		m.put("Path/Right", new SimpleImmutableEntry<Byte, Byte>(new Byte(
 		(byte) 0x03), new Byte((byte) 0x01)));
-		m.put("Trees/Top Left (Grass)", new SimpleImmutableEntry<Byte, Byte>(
-		new Byte((byte) 0x00), new Byte((byte) 0x01)));
-		m.put("Trees/Top Right (Grass)", new SimpleImmutableEntry<Byte, Byte>(
-		new Byte((byte) 0x01), new Byte((byte) 0x01)));
+		m.put("Path/Left", new SimpleImmutableEntry<Byte, Byte>(new Byte(
+		(byte) 0x04), new Byte((byte) 0x01)));
+		m.put("Path/Top_Left", new SimpleImmutableEntry<Byte, Byte>(new Byte(
+		(byte) 0x00), new Byte((byte) 0x02)));
+		m.put("Path/Top_Right", new SimpleImmutableEntry<Byte, Byte>(new Byte(
+		(byte) 0x01), new Byte((byte) 0x02)));
+		m.put("Path/Bottom_Left", new SimpleImmutableEntry<Byte, Byte>(
+		new Byte((byte) 0x02), new Byte((byte) 0x02)));
+		m.put("Path/Bottom_Right", new SimpleImmutableEntry<Byte, Byte>(
+		new Byte((byte) 0x03), new Byte((byte) 0x02)));
+		// m.put("Soil/Grass", new SimpleImmutableEntry<Byte, Byte>(new Byte(
+		// (byte) 0x00), new Byte((byte) 0x00)));
+		// m.put("Soil/Soil", new SimpleImmutableEntry<Byte, Byte>(new Byte(
+		// (byte) 0x01), new Byte((byte) 0x00)));
+		// m.put("Path/Top left", new SimpleImmutableEntry<Byte, Byte>(new Byte(
+		// (byte) 0x02), new Byte((byte) 0x00)));
+		// m.put("Path/Top", new SimpleImmutableEntry<Byte, Byte>(new Byte(
+		// (byte) 0x03), new Byte((byte) 0x00)));
+		// m.put("Path/Bottom Left", new SimpleImmutableEntry<Byte, Byte>(
+		// new Byte((byte) 0x02), new Byte((byte) 0x01)));
+		// m.put("Path/Bottom", new SimpleImmutableEntry<Byte, Byte>(new Byte(
+		// (byte) 0x03), new Byte((byte) 0x01)));
+		// m.put("Trees/Top Left (Grass)", new SimpleImmutableEntry<Byte, Byte>(
+		// new Byte((byte) 0x00), new Byte((byte) 0x01)));
+		// m.put("Trees/Top Right (Grass)", new SimpleImmutableEntry<Byte,
+		// Byte>(
+		// new Byte((byte) 0x01), new Byte((byte) 0x01)));
 
 		try
 		{
 			ObjectOutputStream os = new ObjectOutputStream(
-			new FileOutputStream("test.dspr"));
+			new FileOutputStream("prueba.dspr"));
 			os.writeObject(m);
 			os.close();
 		}
